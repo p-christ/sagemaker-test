c6efbe6c246fccd88415f79bfe20c4d66fb0ea65
Petros Christodoulou <petrochr@amazon.com>
2020-04-28 13:32:21 +0100

    generate

--- pytorch-rnn-scripts/generate.py
+++ pytorch-rnn-scripts/generate.py
@@ -13,27 +13,30 @@ logger = logging.getLogger(__name__)
 
 
 def model_fn(model_dir):
-    logger.info('Loading the model.')
-    model_info = {}
-    with open(os.path.join(model_dir, 'model_info.pth'), 'rb') as f:
-        model_info = torch.load(f)
-    print('model_info: {}'.format(model_info))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    logger.info('Current device: {}'.format(device))
-    model = RNNModel(rnn_type=model_info['rnn_type'], ntoken=model_info['ntoken'],
-                     ninp=model_info['ninp'], nhid=model_info['nhid'], nlayers=model_info['nlayers'],
-                     dropout=model_info['dropout'], tie_weights=model_info['tie_weights'])
-    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
-        model.load_state_dict(torch.load(f))
-        # after load the rnn params are not a continuous chunk of memory
-        # this makes them a continuous chunk, and will speed up forward pass
-        model.rnn.flatten_parameters()
-    model.to(device).eval()
-    logger.info('Loading the data.')
-    corpus = data.Corpus(model_dir)
-    logger.info('Done loading model and corpus. Corpus dictionary size: {}'.format(
-        len(corpus.dictionary)))
-    return {'model': model, 'corpus': corpus}
+    def meth(a):
+        return "Meth A"
+    return meth
+    # logger.info('Loading the model.')
+    # model_info = {}
+    # with open(os.path.join(model_dir, 'model_info.pth'), 'rb') as f:
+    #     model_info = torch.load(f)
+    # print('model_info: {}'.format(model_info))
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # logger.info('Current device: {}'.format(device))
+    # model = RNNModel(rnn_type=model_info['rnn_type'], ntoken=model_info['ntoken'],
+    #                  ninp=model_info['ninp'], nhid=model_info['nhid'], nlayers=model_info['nlayers'],
+    #                  dropout=model_info['dropout'], tie_weights=model_info['tie_weights'])
+    # with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
+    #     model.load_state_dict(torch.load(f))
+    #     # after load the rnn params are not a continuous chunk of memory
+    #     # this makes them a continuous chunk, and will speed up forward pass
+    #     model.rnn.flatten_parameters()
+    # model.to(device).eval()
+    # logger.info('Loading the data.')
+    # corpus = data.Corpus(model_dir)
+    # logger.info('Done loading model and corpus. Corpus dictionary size: {}'.format(
+    #     len(corpus.dictionary)))
+    # return {'model': model, 'corpus': corpus}
 
 
 def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
@@ -55,34 +58,35 @@ def output_fn(prediction_output, accept=JSON_CONTENT_TYPE):
 
 
 def predict_fn(input_data, model):
-    return "HELLO!!!"
-    logger.info('Generating text based on input parameters.')
-    corpus = model['corpus']
-    model = model['model']
+    x = model()
+    return "HELLO!!! " + x
+    # logger.info('Generating text based on input parameters.')
+    # corpus = model['corpus']
+    # model = model['model']
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    logger.info('Current device: {}'.format(device))
-    torch.manual_seed(input_data['seed'])
-    ntokens = len(corpus.dictionary)
-    input = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
-    hidden = model.init_hidden(1)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # logger.info('Current device: {}'.format(device))
+    # torch.manual_seed(input_data['seed'])
+    # ntokens = len(corpus.dictionary)
+    # input = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
+    # hidden = model.init_hidden(1)
 
-    logger.info('Generating {} words.'.format(input_data['words']))
-    result = []
-    with torch.no_grad():  # no tracking history
-        for i in range(input_data['words']):
-            output, hidden = model(input, hidden)
-            word_weights = output.squeeze().div(
-                input_data['temperature']).exp().cpu()
-            word_idx = torch.multinomial(word_weights, 1)[0]
-            input.fill_(word_idx)
-            word = corpus.dictionary.idx2word[word_idx]
-            word = word if type(word) == str else word.decode()
-            if word == '<eos>':
-                word = '\n'
-            elif i % 12 == 11:
-                word = word + '\n'
-            else:
-                word = word + ' '
-            result.append(word)
-    return ''.join(result)
+    # logger.info('Generating {} words.'.format(input_data['words']))
+    # result = []
+    # with torch.no_grad():  # no tracking history
+    #     for i in range(input_data['words']):
+    #         output, hidden = model(input, hidden)
+    #         word_weights = output.squeeze().div(
+    #             input_data['temperature']).exp().cpu()
+    #         word_idx = torch.multinomial(word_weights, 1)[0]
+    #         input.fill_(word_idx)
+    #         word = corpus.dictionary.idx2word[word_idx]
+    #         word = word if type(word) == str else word.decode()
+    #         if word == '<eos>':
+    #             word = '\n'
+    #         elif i % 12 == 11:
+    #             word = word + '\n'
+    #         else:
+    #             word = word + ' '
+    #         result.append(word)
+    # return ''.join(result)
